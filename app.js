727852e53fa64607262ebdf9273f07036d21bd10
Steven Crickman <spc004@mcdaniel.edu>
2019-09-27 16:38:52 -0400

    Continued work

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 if (process.env.NODE_ENV !== 'production')
 {
-    require('dotenv').load()
+    require('dotenv').config()
 }
 
 const express = require('express');
@@ -9,6 +9,8 @@ const app = express();
 const expressLayouts = require('express-ejs-layouts');
 
 const indexRouter = require('./routes/index');
+
+const authorRouter = require('./routes/authors');
 
 const mongoose = require('mongoose');
 
@@ -24,6 +26,7 @@ app.set('layout', 'layouts/layout');
 app.use(expressLayouts);
 app.use(express.static('public'));
 app.use('/', indexRouter);
+app.use('/authors', authorRouter);
 
 app.listen(process.env.PORT || 3000)
 
---
+++ routes/authors.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const router = express.Router();
+const Author = require('../models/author');
+
+// All authors Route
+router.get('/', (req, res) => 
+{
+    res.render('authors/index')
+})
+
+// New Author Route
+router.get('/new', (req, res) =>
+{
+    res.render('authors/new', { author: new author()})
+})
+
+// Create Author Route
+router.post('/', (req, res) =>
+{
+    res.send('Create')
+});
+
+module.exports = router;
